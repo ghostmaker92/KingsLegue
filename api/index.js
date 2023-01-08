0df379a0fe9ed2abca514ebfdc98068f4428d2ad
ghostmaker92 <dryor.19@gmail.com>
2023-01-08 09:58:32 +0100

    Merge

--- api/index.js
+++ api/index.js
@@ -1,58 +1,207 @@
 import { Hono } from 'hono'
 import { serveStatic } from 'hono/serve-static.module'
-import leaderBoard from '../db/leaderboard.json'
-import presidents from '../db/presidents.json'
+import leaderboard from '../db/leaderboard.json'
 import teams from '../db/teams.json'
+import presidents from '../db/presidents.json'
+import topScorers from '../db/top_scorers.json'
+import coaches from '../db/coaches.json'
+import mvp from '../db/mvp.json'
+import topAssists from '../db/top_assists.json'
+import schedule from '../db/schedule.json'
+import playersTwelve from '../db/players_twelve.json'
+import topStatistics from '../db/top_statistics.json'
 
-const app = new Hono();
+const app = new Hono()
 
-app.get('/', (c) => c.json([
-	{
-		endpoint: '/leaderboard',
-		description: 'Returns the leaderboard'
-	},
-	{
-		endpoint: '/teams',
-		description: 'Returns Kings League teams'
-	},
-	{
-		endpoint: '/presidents',
-		description: 'Returns Kings League presidents'
-	}
-]))
+app.get('/', (ctx) =>
+	ctx.json([
+		{
+			endpoint: '/leaderboard',
+			description: 'Returns Kings League leaderboard',
+			parameters: [
+				{
+					name: 'team',
+					endpoint: '/leaderboard/:teamId',
+					description: 'Return Kings League leaderboard info from Team Id'
+				}
+			]
+		},
+		{
+			endpoint: '/teams',
+			description: 'Returns Kings League teams',
+			parameters: [
+				{
+					name: 'id',
+					endpoint: '/teams/:id',
+					description: 'Return Kings League team by id'
+				}
+			]
+		},
+		{
+			endpoint: '/presidents',
+			description: 'Returns Kings League presidents',
+			parameters: [
+				{
+					name: 'id',
+					endpoint: '/presidents/:id',
+					description: 'Return Kings League president by id'
+				}
+			]
+		},
+		{
+			endpoint: '/coaches',
+			description: 'Returns Kings League coaches',
+			parameters: [
+				{
+					name: 'teamId',
+					endpoint: '/top-assists/:teamId',
+					description: 'Return Kings League coach of team by id of some'
+				}
+			]
+		},
+		{
+			endpoint: '/top-assists',
+			description: 'Returns Kings League Top Assists',
+			parameters: [
+				{
+					name: 'rank',
+					endpoint: '/top-assists/:rank',
+					description: 'Return Kings League top assister by rank'
+				}
+			]
+		},
+		{
+			endpoint: '/top-scorers',
+			description: 'Returns Kings League Top Scorers',
+			parameters: [
+				{
+					name: 'rank',
+					endpoint: '/top-scorers/:rank',
+					description: 'Return Kings League top scorer by rank'
+				}
+			]
+		},
+		{
+			endpoint: '/mvp',
+			description: 'Returns Kings League Most Valuable Players'
+		},
+		{
+			endpoint: '/schedule',
+			description: 'Returns Kings League match schedule and the final score of played games.'
+		},
+		{
+			endpoint: '/players-12',
+			description: 'Returns Kings League Players Twelve'
+		}
+	])
+)
 
-app.get('/leaderboard', (c) => {
-	return c.json(leaderBoard)
+app.get('/leaderboard', (ctx) => {
+	return ctx.json(leaderboard)
 })
 
-app.get('/presidents', (c) => {
-	return c.json(presidents)
+app.get('/leaderboard/:teamId', (ctx) => {
+	const teamId = ctx.req.param('teamId')
+	const foundTeam = leaderboard.find((stats) => stats.team.id === teamId)
+
+	return foundTeam ? ctx.json(foundTeam) : ctx.json({ message: 'Team not found' }, 404)
 })
 
-app.get('/presidents/:id', (c) => {
-	const id = c.req.param('id')
-	const foundPresident = presidents.find(president => president.id === id)
-	return foundPresident ? c.json(foundPresident) : c.json({message: 'President not found'}, 404)
+app.get('/teams', (ctx) => {
+	return ctx.json(teams)
 })
 
-app.get('/teams', (c) => {
-	return c.json(teams)
+app.get('/presidents', (ctx) => {
+	return ctx.json(presidents)
 })
 
-app.get('/teams/:id', (c) => {
-	const id = c.req.param('id')
-	const foundTeam = teams.find(team => team.id === id)
-	return foundTeam ? c.json(foundTeam) : c.json({message: 'Team not found'}, 404)
+app.get('/top-statistics', (ctx) => {
+	return ctx.json(topStatistics)
 })
 
-app.get('/static/*', serveStatic({root: './'}))
+app.get('/top-scorers', (ctx) => {
+	return ctx.json(topScorers)
+})
+
+app.get('/top-scorers/:rank', (ctx) => {
+	const ranking = ctx.req.param('rank')
+	const foundScorer = topScorers.find((scorer) => scorer.ranking === ranking)
+
+	return foundScorer ? ctx.json(foundScorer) : ctx.json({ message: 'Top scorer not found' }, 404)
+})
+
+app.get('/top-assists', (ctx) => {
+	return ctx.json(topAssists)
+})
+
+app.get('/top-assists/:rank', (ctx) => {
+	const ranking = ctx.req.param('rank')
+	const foundAssister = topAssists.find((assister) => assister.rank === ranking)
+
+	return foundAssister
+		? ctx.json(foundAssister)
+		: ctx.json({ message: 'Top assister not found' }, 404)
+})
+
+app.get('/mvp', (ctx) => {
+	return ctx.json(mvp)
+})
+
+app.get('/coaches', (ctx) => {
+	return ctx.json(coaches)
+})
+
+app.get('/coaches/:teamId', (ctx) => {
+	const teamId = ctx.req.param('teamId')
+	const teamName = teams.find((team) => team.id === teamId)
+	const foundedCoach = coaches.find((coach) => coach.teamName === teamName)
+
+	return foundedCoach ? ctx.json(foundedCoach) : ctx.json({ message: 'Coach not found' }, 404)
+})
+
+app.get('/presidents/:id', (ctx) => {
+	const id = ctx.req.param('id')
+	const foundPresident = presidents.find((president) => president.id === id)
+
+	return foundPresident
+		? ctx.json(foundPresident)
+		: ctx.json({ message: 'President not found' }, 404)
+})
+
+app.get('/teams/:id', (ctx) => {
+	const id = ctx.req.param('id')
+	const foundTeam = teams.find((team) => team.id === id)
+
+	return foundTeam ? ctx.json(foundTeam) : ctx.json({ message: 'Team not found' }, 404)
+})
+
+app.get('/schedule', (ctx) => {
+	return ctx.json(schedule)
+})
+
+app.get('/teams/:id/players-12', (ctx) => {
+	const id = ctx.req.param('id')
+	const foundPlayerTwelve = playersTwelve.filter((player) => player.team.id === id)
+
+	return foundPlayerTwelve
+		? ctx.json(foundPlayerTwelve)
+		: ctx.json({ message: `Players for team ${id} not found` }, 404)
+})
+
+app.get('/players-12', (ctx) => {
+	return ctx.json(playersTwelve)
+})
+
+app.get('/static/*', serveStatic({ root: './' }))
 
 app.notFound((c) => {
-	if(c.req.url.at(-1) === '/'){
-		return c.redirect(pahtname.slice(0, -1))
+	const { pathname } = new URL(c.req.url)
+
+	if (c.req.url.at(-1) === '/') {
+		return c.redirect(pathname.slice(0, -1))
 	}
-	return c.json({message: 'Not Found'}, 404)
+
+	return c.json({ message: 'Not Found' }, 404)
 })
 
 export default app
-
--- api/index.test.js
+++ api/index.test.js
@@ -1,26 +1,319 @@
-import { unstable_dev } from "wrangler";
-import { describe, expect, it, beforeAll, afterAll } from "vitest";
+import { unstable_dev as unstableDev } from 'wrangler'
+import { describe, expect, it, beforeAll, afterAll } from 'vitest'
 
-describe("Worker", () => {
-	let worker;
+const setup = async () => {
+	const worker = await unstableDev('api/index.js', {}, { disableExperimentalWarning: true })
+	return worker
+}
+
+const teardown = async (worker) => {
+	await worker.stop()
+}
+
+describe('Testing / route', () => {
+	let worker
 
 	beforeAll(async () => {
-		worker = await unstable_dev(
-			"src/index.js",
-			{},
-			{ disableExperimentalWarning: true }
-		);
-	});
+		worker = await setup()
+	})
 
 	afterAll(async () => {
-		await worker.stop();
-	});
+		await teardown(worker)
+	})
 
-	it("should return Hello World", async () => {
-		const resp = await worker.fetch();
-		if (resp) {
-			const text = await resp.text();
-			expect(text).toMatchInlineSnapshot(`"Hello World!"`);
+	it('routes should have endpoint and description', async () => {
+		const res = await worker.fetch()
+		expect(res).toBeDefined()
+		if (!res) return
+
+		const apiRoutes = await res.json()
+		// verify the response to have the expected format
+		apiRoutes.forEach((endpoint) => {
+			expect(endpoint).toHaveProperty('endpoint')
+			expect(endpoint).toHaveProperty('description')
+		})
+	})
+})
+
+describe('Testing /teams route', () => {
+	let worker
+
+	beforeAll(async () => {
+		worker = await setup()
+	})
+
+	afterAll(async () => {
+		await teardown(worker)
+	})
+
+	it('The teams should have all teams', async () => {
+		const resp = await worker.fetch('/teams')
+		expect(resp).toBeDefined()
+		if (!resp) return
+
+		const teams = await resp.json()
+		const numberTeams = Object.entries(teams).length
+
+		// verify the team have all props
+		teams.forEach((team) => {
+			expect(team).toHaveProperty('id')
+			expect(team).toHaveProperty('name')
+			expect(team).toHaveProperty('image')
+			expect(team).toHaveProperty('url')
+			expect(team).toHaveProperty('presidentId')
+			expect(team).toHaveProperty('channel')
+			expect(team).toHaveProperty('coach')
+			expect(team).toHaveProperty('socialNetworks')
+			expect(team).toHaveProperty('players')
+		})
+
+		expect(numberTeams).toBe(12)
+	})
+
+	it('Get /teams/1k should return team props', async () => {
+		const resp = await worker.fetch('/teams/1k')
+		expect(resp).toBeDefined()
+		if (!resp) return
+
+		const team = await resp.json()
+
+		expect(team).toHaveProperty('id')
+		expect(team).toHaveProperty('name')
+		expect(team).toHaveProperty('image')
+		expect(team).toHaveProperty('url')
+		expect(team).toHaveProperty('presidentId')
+		expect(team).toHaveProperty('channel')
+		expect(team).toHaveProperty('coach')
+		expect(team).toHaveProperty('socialNetworks')
+		expect(team).toHaveProperty('players')
+	})
+
+	it('Get /teams/noexist should return 404 message missing team', async () => {
+		const resp = await worker.fetch('/teams/noexist')
+		expect(resp).toBeDefined()
+		if (!resp) return
+
+		const errorMessage = await resp.json()
+
+		expect(errorMessage).toEqual({
+			message: 'Team not found'
+		})
+	})
+})
+
+describe('Testing /presidents route', () => {
+	let worker
+
+	beforeAll(async () => {
+		worker = await setup()
+	})
+
+	afterAll(async () => {
+		await teardown(worker)
+	})
+
+	it('should return presidents', async () => {
+		const resp = await worker.fetch('/presidents')
+		expect(resp).toBeDefined()
+		if (!resp) return
+
+		const presidents = await resp.json()
+		const numberPresidents = Object.entries(presidents).length
+
+		// verify the team have all props
+		presidents.forEach((president) => {
+			expect(president).toHaveProperty('id')
+			expect(president).toHaveProperty('name')
+			expect(president).toHaveProperty('image')
+			expect(president).toHaveProperty('teamId')
+		})
+
+		expect(numberPresidents).toBe(12)
+	})
+
+	it('should return presidente details by id', async () => {
+		const resp = await worker.fetch('/presidents/iker-casillas')
+		expect(resp).toBeDefined()
+		if (!resp) return
+
+		const president = await resp.json()
+		const iker = {
+			id: 'iker-casillas',
+			name: 'Iker Casillas',
+			image: 'https://kingsleague.dev/presidents/iker-casillas.webp',
+			teamId: '1k'
 		}
-	});
-});
+
+		expect(president).toHaveProperty('id')
+		expect(president).toHaveProperty('name')
+		expect(president).toHaveProperty('image')
+		expect(president).toHaveProperty('teamId')
+		expect(president).toEqual(iker)
+	})
+
+	it('should return 404 and message missing team with no exist president', async () => {
+		const resp = await worker.fetch('/presidents/noexist')
+		expect(resp).toBeDefined()
+		if (!resp) return
+
+		const errorMessage = await resp.json()
+
+		expect(errorMessage).toEqual({
+			message: 'President not found'
+		})
+	})
+})
+
+describe('Test /schedule route', () => {
+	let worker
+
+	beforeAll(async () => {
+		worker = await setup()
+	})
+
+	afterAll(async () => {
+		await teardown(worker)
+	})
+
+	it('Should return 11 days', async () => {
+		const resp = await worker.fetch('/schedule')
+		expect(resp).toBeDefined()
+
+		const days = await resp.json()
+		expect(days).toHaveLength(11)
+	})
+
+	it('Days should have their date and matches', async () => {
+		const resp = await worker.fetch('/schedule')
+		const days = await resp.json()
+		const properties = ['date', 'matches']
+
+		days.forEach((day) => {
+			properties.forEach((property) => {
+				expect(day).toHaveProperty(property)
+			})
+		})
+	})
+
+	it('Matches should have all their properties', async () => {
+		const resp = await worker.fetch('/schedule')
+		const days = await resp.json()
+		const matches = days.map((day) => day.matches).flat()
+		const properties = ['timestamp', 'hour', 'teams', 'score']
+
+		matches.forEach((match) => {
+			properties.forEach((property) => {
+				expect(match).toHaveProperty(property)
+			})
+		})
+	})
+
+	it('Teams should have all their properties', async () => {
+		const resp = await worker.fetch('/schedule')
+		const days = await resp.json()
+
+		const teams = days
+			.map((day) => day.matches)
+			.flat()
+			.map((match) => match.teams)
+			.flat()
+
+		const properties = ['id', 'name', 'shortName']
+
+		teams.forEach((team) => {
+			properties.forEach((property) => {
+				expect(team).toHaveProperty(property)
+			})
+		})
+	})
+})
+
+describe('Testing /leaderboard route', () => {
+	let worker
+
+	const entryProperties = [
+		'wins',
+		'losses',
+		'scoredGoals',
+		'concededGoals',
+		'yellowCards',
+		'redCards',
+		'team',
+		'rank'
+	]
+
+	const nestedTeamProperties = [
+		'color',
+		'id',
+		'name',
+		'image',
+		'imageWhite',
+		'url',
+		'channel',
+		'socialNetworks',
+		'players',
+		'coach',
+		'shortName',
+		'coachInfo',
+		'president'
+	]
+
+	beforeAll(async () => {
+		worker = await setup()
+	})
+
+	afterAll(async () => {
+		await teardown(worker)
+	})
+
+	it('Should return 12 teams', async () => {
+		const resp = await worker.fetch('/leaderboard')
+		expect(resp).toBeDefined()
+
+		const leaderboard = await resp.json()
+		expect(leaderboard).toHaveLength(12)
+	})
+
+	it('Entries should have all their properties', async () => {
+		const resp = await worker.fetch('/leaderboard')
+		const leaderboard = await resp.json()
+
+		leaderboard.forEach((entry) => {
+			entryProperties.forEach((property) => {
+				expect(entry).toHaveProperty(property)
+			})
+		})
+	})
+
+	it('Teams should have all their properties', async () => {
+		const resp = await worker.fetch('/leaderboard')
+		const leaderboard = await resp.json()
+		const teams = leaderboard.map((entry) => entry.team)
+
+		teams.forEach((team) => {
+			nestedTeamProperties.forEach((property) => {
+				expect(team).toHaveProperty(property)
+			})
+		})
+	})
+
+	it('Shoud return a team from its id', async () => {
+		const resp = await worker.fetch('/leaderboard/1k')
+		expect(resp).toBeDefined()
+		const entry = await resp.json()
+		const { team } = entry
+
+		entryProperties.forEach((property) => expect(entry).toHaveProperty(property))
+		nestedTeamProperties.forEach((property) => expect(team).toHaveProperty(property))
+	})
+
+	it("Should return 404 message when the id doesn't exists", async () => {
+		const resp = await worker.fetch('/leaderboard/midudev')
+		expect(resp).toBeDefined()
+		const errorMessage = await resp.json()
+
+		expect(errorMessage).toEqual({
+			message: 'Team not found'
+		})
+	})
+})
