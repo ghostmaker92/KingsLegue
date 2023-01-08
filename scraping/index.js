e58c56c5db62ca7125095789bac933997a90e600
ghostmaker92 <dryor.19@gmail.com>
2023-01-08 01:49:46 +0100

    1.0.0.1

---
+++ scraping/index.js
@@ -0,0 +1,65 @@
+import * as cheerio from 'cheerio'
+import {writeFile, readFile} from 'node:fs/promises'
+import path from 'node:path'
+
+const DB_PATH = path.join(process.cwd(), './db/')
+const TEAMS = await readFile(`${DB_PATH}/teams.json`, 'utf-8').then(JSON.parse)
+
+//import TEAMS from '../db/teams.json' assert {type: 'json'}
+
+const URLS ={
+    leaderBoard: 'https://kingsleague.pro/estadisticas/clasificacion/'
+}
+
+async function scrape (url){
+    const res = await fetch(url) 
+    const html = await res.text()
+    return cheerio.load(html)
+}
+
+async function getLeaderBoard(){
+    const $ = await scrape(URLS.leaderBoard)
+    const $rows = $('table tbody tr')
+
+    const LEADERBOARD_SELECTORS = {
+        team: {selector: '.fs-table-text_3', typeOf: 'string'},
+        wins: {selector: '.fs-table-text_4', typeOf: 'number'},
+        loses: {selector: '.fs-table-text_5', typeOf: 'number'},
+        scoredGoals: {selector: '.fs-table-text_6', typeOf: 'number'},
+        concededGoals: {selector: '.fs-table-text_7', typeOf: 'number'},
+        yellowCards: {selector: '.fs-table-text_8', typeOf: 'number'},
+        redCards: {selector: '.fs-table-text_9', typeOf: 'number'},
+    }
+
+    const getTeamFrom = ({name}) => TEAMS.find(team => team.name === name )
+
+    const cleanText = text => text
+        .replace(/\t|\n|\s:/g, '')
+        .replace(/.*:/g,' ')
+        .trim() 
+
+    const leaderBoardSelectorEntries = Object.entries(LEADERBOARD_SELECTORS)
+
+    const leaderBoard = [] 
+    $rows.each((_, el) => {
+        const leaderBoardEntries = leaderBoardSelectorEntries.map(([key, {selector, typeOf}]) => {
+            const rawValue  = $(el).find(selector).text()
+            const cleanedValue = cleanText(rawValue)
+
+            const value = typeOf === 'number' ? Number(cleanedValue) : cleanedValue
+            return [key, value]
+        })
+        const{ team: teamName, ...leaderBoardForTeam} = Object.fromEntries(leaderBoardEntries)
+        const team = getTeamFrom({name: teamName}) 
+
+        leaderBoard.push({
+            ...leaderBoardForTeam,
+            team
+        })
+    })
+    return leaderBoard
+}
+
+const leaderBoard = await getLeaderBoard()
+await writeFile(`${DB_PATH}/leaderboard.json`, JSON.stringify(leaderBoard, null, 2), 'utf-8')
+ 
